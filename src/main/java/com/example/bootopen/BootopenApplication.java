badc8bbf60c249d214f586654f6bd51a14896e15
jidd <chaijindong>
2018-12-26 16:43:56 +0800

    the boot git

---
+++ src/main/java/com/example/bootopen/BootopenApplication.java
@@ -0,0 +1,14 @@
+package com.example.bootopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BootopenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootopenApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/bootopen/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.bootopen.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping("/hello")
+    private String index() {
+        return "hello world";
+    }
+}
