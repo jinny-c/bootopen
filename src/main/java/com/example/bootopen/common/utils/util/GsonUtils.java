b6b5508032a03aae7da71dfbe67816d4be210053
jidd <chaijindong>
2022-07-13 11:23:50 +0800

    数据转换

--- src/main/java/com/example/bootopen/common/utils/util/GsonUtils.java
+++ src/main/java/com/example/bootopen/common/utils/util/GsonUtils.java
@@ -2,6 +2,7 @@ package com.example.bootopen.common.utils.util;
 
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import lombok.SneakyThrows;
 
 import java.lang.reflect.Type;
 import java.util.Map;
@@ -12,8 +13,14 @@ public class GsonUtils {
     private static Gson gsonWithoutAnnotation = null;
 
     static {
-        gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().enableComplexMapKeySerialization().serializeNulls()
-                .disableHtmlEscaping().setDateFormat("yyyyMMddHHmmss").setVersion(1.00).setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        gson = new GsonBuilder()
+                //在序列化或反序列化时 排除 所有 没有被 @Expose 注解的字段。
+                .excludeFieldsWithoutExposeAnnotation()
+                .enableComplexMapKeySerialization().serializeNulls()
+                .disableHtmlEscaping()
+                .setDateFormat("yyyyMMddHHmmss")
+                .setVersion(1.00)
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         gsonWithoutAnnotation = new GsonBuilder().enableComplexMapKeySerialization().setDateFormat("yyyyMMddHHmmss").setVersion(1.00)
                 .disableHtmlEscaping().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
     }
@@ -54,13 +61,14 @@ public class GsonUtils {
         return gsonWithoutAnnotation.fromJson(str, type);
     }
 
+    @SneakyThrows
     public static Map<String, Object> toMap(Object obj) {
         Type type = new TypeToken<Map<String, Object>>() {
         }.getType();
         return gsonWithoutAnnotation.fromJson(toJson(obj), type);
     }
 
-
+    @SneakyThrows(JsonSyntaxException.class)
     public static Map<String, String> toStrMap(Object obj) {
         Type type = new TypeToken<Map<String, String>>() {
         }.getType();
