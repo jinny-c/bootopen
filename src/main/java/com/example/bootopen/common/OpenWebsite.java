01cfd53de6ff59538950a56479ae3fcd6fc8e949
jidd <chaijindong>
2023-06-12 10:12:01 +0800

    web项目启动后打开浏览器
    Signed-off-by: jidd <chaijindong>

---
+++ src/main/java/com/example/bootopen/common/OpenWebsite.java
@@ -0,0 +1,51 @@
+package com.example.bootopen.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+/**
+ * @description 启动后自动打开网站
+ * @auth chaijd
+ * @date 2023/6/12
+ */
+@Component
+@Slf4j
+public class OpenWebsite implements CommandLineRunner {
+    private static final String[] WINDOWS_CMD = {"cmd", "/c", "start"};
+    private static final String[] LINUX_CMD = {"/usr/bin/xdg-open"};
+
+    @Override
+    public void run(String... args) throws IOException, URISyntaxException {
+        String url = "http://localhost:8089";
+        if (Desktop.isDesktopSupported()) {  // 检查是否支持桌面操作
+            Desktop desktop = Desktop.getDesktop();
+            if (desktop.isSupported(Desktop.Action.BROWSE)) {  // 检查是否支持浏览器打开
+                desktop.browse(new URI(url));
+            } else {
+                openUrlWithCommand(url);
+            }
+        } else {
+            openUrlWithCommand(url);
+        }
+    }
+
+    private void openUrlWithCommand(String url) throws IOException {
+        String[] cmd;
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            cmd = WINDOWS_CMD;
+        } else {
+            cmd = LINUX_CMD;
+        }
+        cmd = Arrays.copyOf(cmd, cmd.length + 1);
+        cmd[cmd.length - 1] = url;
+        log.info("openUrlWithCommand cmd={}", Arrays.asList(cmd));
+        Runtime.getRuntime().exec(cmd);
+    }
+}
